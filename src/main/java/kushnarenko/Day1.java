b5011a21ff489f5940e577bc36c41b22398bfcc2
KushnarenkoAlex <kushnarenko.alex@gmail.com>
2022-12-06 19:54:31 +0000

    D1P1

---
+++ src/main/java/kushnarenko/Day1.java
@@ -0,0 +1,30 @@
+package kushnarenko;
+
+import com.google.common.io.Resources;
+
+import java.io.IOException;
+import java.net.URL;
+import java.nio.charset.StandardCharsets;
+import java.util.Arrays;
+import java.util.List;
+
+public class Day1 {
+    public String[] input;
+
+    public Day1() {
+        try {
+            URL url = Resources.getResource("Day1Input.txt");
+            this.input = Resources.toString(url, StandardCharsets.UTF_8).split("\n\n");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public int part1() {
+        return Arrays.stream(input).map(s ->
+                        Arrays.stream(s.split("\n"))
+                                .mapToInt(Integer::parseInt)
+                                .sum())
+                .max(Integer::compare).orElse(0);
+    }
+}
---
+++ src/test/java/kushnarenko/Day1Test.java
@@ -0,0 +1,15 @@
+package kushnarenko;
+
+import org.junit.jupiter.api.Test;
+
+public class Day1Test {
+    @Test
+    void testDay1Part1() {
+        System.out.println(new Day1().part1());
+    }
+
+    @Test
+    void testDay1Part2() {
+        System.out.println(new Day1().part1());
+    }
+}
